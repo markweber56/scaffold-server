f6b50301cee6af897d7d4a7fd5d0326bd6c12d28
mark weber <markweber56@gmail.com>
2024-04-19 23:21:30 -0500

    set port, add route for root

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_cors import CORS
 from random import randint
@@ -10,6 +12,10 @@ allowed_origins = [
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": allowed_origins}})
 
+@app.route('/', methods=['GET'])
+def home():
+    return {"message": "you're home!"}
+
 @app.route('/api/data', methods=['GET'])
 def get_data():
     random_integer = randint(1, 100)
@@ -18,4 +24,5 @@ def get_data():
     return data
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use port provided by Heroku or default to 5000
+    app.run(host='0.0.0.0', port=port)
