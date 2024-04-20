e6ce9a7216abe28a9900f6c777862aa75b989beb
mark weber <markweber56@gmail.com>
2024-04-19 22:54:07 -0500

    return a random integer

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from flask_cors import CORS
+from random import randint
+
+allowed_origins = [
+    "http://localhost:3000",
+    "https://scaffold-web-c43c33ecc8e5.herokuapp.com/"
+]
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
+cors = CORS(app, resources={r"/api/*": {"origins": allowed_origins}})
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
-    data = {"abc": 123}
+    random_integer = randint(1, 100)
+    data = {"abc": f"here's a random integer: {random_integer}"}
 
     return data
 
