8c6b893fed98426c86d455906e105e6fae517f6c
mark weber <markweber56@gmail.com>
2024-04-19 23:37:59 -0500

    git add logic for cors check

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 import os
 
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 from random import randint
 
 allowed_origins = [
     "http://localhost:3000",
-    "https://scaffold-web-c43c33ecc8e5.herokuapp.com"
+    "https://scaffold-web"
 ]
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/api/*": {"origins": allowed_origins}})
+# cors = CORS(app, resources={r"/api/*": {"origins": allowed_origins}})
+CORS(app)
 
 @app.route('/', methods=['GET'])
 def home():
@@ -22,6 +23,21 @@ def get_data():
     data = {"abc": f"here's a random integer: {random_integer}"}
 
     return data
+
+@app.after_request
+def apply_cors(response):
+    allowed_origin = determine_allowed_origin(request.headers.get('Origin'))
+    response.headers.add('Access-Control-Allow-Origin', allowed_origin)
+    return response
+
+def determine_allowed_origin(origin):
+    # Check if the origin matches the pattern you want to allow
+    for allowed_origin in allowed_origins:
+        if origin and origin.startswith(allowed_origin):
+            print(f'origin: {origin} is allowed')
+            return origin
+    print(f'origin {origin} not allowed')
+    return 'null' # Deny requests from other origins
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))  # Use port provided by Heroku or default to 5000
